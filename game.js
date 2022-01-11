016174da7ca388e1ebd7db29771fef8b95a3eb41
Dominik Dvořák <65647273+Lambdacorporation@users.noreply.github.com>
2022-01-11 23:04:00 +0100

    Update game.js
    Docela málo změn. Přidání zobrazení pop up okna.

--- game.js
+++ game.js
@@ -1,15 +1,26 @@
+//definování cen
+var cena_oken = 10600000;
 //výdělek klikáním
 var coiny = 0;
-var income = 1;
+var prijem = 1;
 var sklonovani = "coinů";
 function pracuj(){
-    coiny += income;
+    coiny += prijem;
     if (coiny == 1) sklonovani = "coin";
     if ((coiny >= 2)&&(coiny < 5)) sklonovani = "coiny";
     if (coiny >= 5) sklonovani = "coinů";
     document.getElementById("coiny").innerHTML = coiny + " " + sklonovani;
     return;
 }
+function ok_penize(){
+    document.getElementById("chybapenize").style.visibility = "hidden";
+}
 //výpočet výdělku
 var vydelek = 0;
 document.getElementById("vydelek").innerHTML = vydelek;
+
+//Hráč si kupuje okna
+function postav_okna(){
+    if (coiny < cena_oken){
+        document.getElementById("chybapenize").style.visibility = "visible";}
+}
