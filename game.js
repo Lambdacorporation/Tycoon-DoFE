7decbf359eead984094ec24b117121ed03b74207
Dominik Dvořák <65647273+Lambdacorporation@users.noreply.github.com>
2022-01-17 16:36:04 +0100

    Update game.js
    Tady přibyli nové funkce k novým prvkům na stránce.

--- game.js
+++ game.js
@@ -1,5 +1,23 @@
 //definování cen
 var cena_oken = 10600000;
+
+//Hráč chce rozbalit nebo skrýt obrázek budovy
+var skryt = 1;
+function budova_skryt(){
+    if (skryt > 0){
+        skryt = 0;
+        document.getElementById("budova_skryt").innerHTML = "˅ rozbalit"; 
+        document.getElementById("budova_obraz").style.display = "none";
+        document.getElementById("budova").style.height = auto;
+    }
+    if (skryt < 1){
+        skryt = 1;
+        document.getElementById("budova_obraz").style.display = "";
+        document.getElementById("budova_skryt").innerHTML = "˄ skrýt";
+        document.getElementById("budova").style.height = auto;
+    }
+}
+
 //výdělek klikáním
 var coiny = 0;
 var prijem = 1;
@@ -12,9 +30,6 @@ function pracuj(){
     document.getElementById("coiny").innerHTML = coiny + " " + sklonovani;
     return;
 }
-function ok_penize(){
-    document.getElementById("chybapenize").style.visibility = "hidden";
-}
 //výpočet výdělku
 var vydelek = 0;
 document.getElementById("vydelek").innerHTML = vydelek;
@@ -23,4 +38,7 @@ document.getElementById("vydelek").innerHTML = vydelek;
 function postav_okna(){
     if (coiny < cena_oken){
         document.getElementById("chybapenize").style.visibility = "visible";}
+}
+function ok_penize(){
+    document.getElementById("chybapenize").style.visibility = "hidden";
 }
